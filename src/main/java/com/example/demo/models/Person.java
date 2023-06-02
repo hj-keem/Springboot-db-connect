6d7ec974d1f97204d0a804d65b88c48932dab91d
김현정 <hyeomjeom@gmail.com>
2023-06-02 14:31:28 +0900

    feat : REST로 JPA 데이터에 엑세스

--- src/main/java/com/example/demo/models/Person.java
+++ src/main/java/com/example/demo/models/Person.java
@@ -1,5 +1,34 @@
 package com.example.demo.models;
 
-public class Person {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-}
+    @Entity
+    public class Person {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+
+        private String firstName;
+        private String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
+
