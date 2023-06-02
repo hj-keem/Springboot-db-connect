932fb405f6abe3be577072a07ac3175a5b8722cb
김현정 <hyeomjeom@gmail.com>
2023-06-02 14:16:32 +0900

    feat : Spring jpa sqlite에 데이터 저장(FindAll, findById, findByLastName)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,57 @@
 package com.example.demo;
 
+import com.example.demo.models.Customer;
+import com.example.demo.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return "Hello " + name;
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return args -> {
+			repository.save(new Customer("leo", "aa"));
+			repository.save(new Customer("yui", "bb"));
+			repository.save(new Customer("jun", "cc"));
+			repository.save(new Customer("leo2", "aa"));
+
+			log.info("findAll-----------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
+			log.info("findById-----------------------------");
+			Customer customer = repository.findById(1L);
+			log.info(customer.toString());
+			log.info("");
+
+			log.info("findByName-----------------------------");
+			repository.findByLastName("aa").forEach(c -> {log.info(c.toString());});
+			log.info(customer.toString());
+		};
+
+	}
+
 }
+
